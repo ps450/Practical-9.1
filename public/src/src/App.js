0c71e62fe287b5a748d311b1b8644ef5e74c5823
ps450 <parshrma17@gmail.com>
2025-11-04 22:47:09 +0530

    Create App.js

---
+++ public/src/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+      <p>Your React app is running inside a Docker container served by Nginx!</p>
+    </div>
+  );
+}
+
+export default App;
